77ac9bfcd7350b9b0203c6bbff8b1d59ce53a150
Mikael <mikael@sehlin.net>
2020-02-16 10:37:06 +0100

    Adding Controller and Injecting Services

---
+++ CandyStore/Controllers/CandyController.cs
@@ -0,0 +1,21 @@
+﻿using CandyStore.Models;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CandyStore.Controllers
+{
+    public class CandyController : Controller
+    {
+        private readonly ICandyRepository _candyRepository;
+        private readonly ICategoryRepository _categoryRepository;
+
+        public CandyController(ICandyRepository candyRepository, ICategoryRepository categoryRepository)
+        {
+            _candyRepository = candyRepository;
+            _categoryRepository = candyRepository;
+        }
+    }
+}
