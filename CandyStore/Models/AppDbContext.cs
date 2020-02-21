4e29a30b29c913ef22c647ad0a87b5f254d07d07
Mikael <mikael@sehlin.net>
2020-02-21 04:02:48 +0100

    Adding Entity Framework and DbContext

---
+++ CandyStore/Models/AppDbContext.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CandyStore.Models
+{
+    public class AppDbContext : DbContext    //Must inherit from DbContext
+    {
+        public AppDbContext(DbContextOptions<AppDbContext> options) : base(options)
+        {
+        }
+
+        public DbSet<Candy> Candies { get; set; }
+        public DbSet<Category> Categories { get; set; }
+    }
+}
