b4ac9d3bb44545d1c9cb874d64f12745450c33a9
Mikael <mikael@sehlin.net>
2020-02-25 00:32:05 +0100

    Creaing Home Controller and View

---
+++ CandyStore/Controllers/HomeController.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using CandyStore.Models;
+using CandyStore.ViewModels;
+using Microsoft.AspNetCore.Mvc;
+
+namespace CandyStore.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ICandyRepository _candyRepository;
+
+        public HomeController(ICandyRepository candyRepository)
+        {
+            _candyRepository = candyRepository;
+        }
+        public IActionResult Index()
+        {
+            var homeViewModel = new HomeViewModel
+            {
+                CandyOnSale = _candyRepository.GetCandyOnSale
+            };
+            return View(homeViewModel);
+        }
+    }
+}
---
+++ CandyStore/ViewModels/HomeViewModel.cs
@@ -0,0 +1,13 @@
+﻿using CandyStore.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CandyStore.ViewModels
+{
+    public class HomeViewModel
+    {
+        public IEnumerable<Candy> CandyOnSale { get; set; }
+    }
+}
