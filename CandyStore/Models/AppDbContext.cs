71e2a47ef5d7a29df59d3557f8df3c4592f24227
Mikael <mikael@sehlin.net>
2020-02-25 15:59:47 +0100

    Adding Shopping Cart Item

--- CandyStore/Models/AppDbContext.cs
+++ CandyStore/Models/AppDbContext.cs
@@ -14,6 +14,7 @@ namespace CandyStore.Models
 
         public DbSet<Candy> Candies { get; set; }
         public DbSet<Category> Categories { get; set; }
+        public DbSet<ShoppingCartItem> ShoppingCartItems { get; set; }
 
         protected override void OnModelCreating(ModelBuilder modelBuilder)
         {
