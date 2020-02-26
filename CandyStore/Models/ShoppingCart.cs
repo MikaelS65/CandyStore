79f9e70a7467f0a1733d88d0421528b5af7604c8
Mikael <mikael@sehlin.net>
2020-02-26 14:01:52 +0100

    Removing Item from the Cart

--- CandyStore/Models/ShoppingCart.cs
+++ CandyStore/Models/ShoppingCart.cs
@@ -62,5 +62,31 @@ namespace CandyStore.Models
 
             _appDbContext.SaveChanges();
         }
+
+        public int RemoveFromCart(Candy candy)
+        {
+            var shoppingCartItem = _appDbContext.ShoppingCartItems.SingleOrDefault(s =>
+                s.Candy.CandyId == candy.CandyId &&
+                s.ShoppingCartId == ShoppingCartId);
+
+            var localAmount = 0;
+
+            if(shoppingCartItem != null)
+            {
+                if(shoppingCartItem.Amount > 1) //will NOT remove Item from Cart
+                {
+                    shoppingCartItem.Amount--;
+                    localAmount = shoppingCartItem.Amount;
+                }
+                else    //WILL remove Item from Cart
+                {
+                    _appDbContext.ShoppingCartItems.Remove(shoppingCartItem);
+                }
+            }
+
+            _appDbContext.SaveChanges();
+
+            return localAmount;
+        }
     }
 }
