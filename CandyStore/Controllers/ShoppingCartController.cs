3f0bd6006434c38f105690fd5f70ba43c9f23a97
Mikael <mikael@sehlin.net>
2020-03-02 03:30:54 +0100

    Adding/Removing Candy To/From the Cart

--- CandyStore/Controllers/ShoppingCartController.cs
+++ CandyStore/Controllers/ShoppingCartController.cs
@@ -31,5 +31,31 @@ namespace CandyStore.Controllers
 
             return View(shoppingCartViewModel);
         }
+
+        public RedirectToActionResult AddToShoppingCart(int candyId)
+        {
+            var selectedCandy = _candyRepository.GetAllCandy
+                .FirstOrDefault(c => c.CandyId == candyId);
+
+            if(selectedCandy != null)
+            {
+                _shoppingCart.AddToCart(selectedCandy, 1);
+            }
+
+            return RedirectToAction("Index");
+        }
+
+        public RedirectToActionResult RemoveFromShoppingCart(int candyId)
+        {
+            var selectedCandy = _candyRepository.GetAllCandy
+                .FirstOrDefault(c => c.CandyId == candyId);
+
+            if (selectedCandy != null)
+            {
+                _shoppingCart.RemoveFromCart(selectedCandy);
+            }
+
+            return RedirectToAction("Index");
+        }
     }
 }
