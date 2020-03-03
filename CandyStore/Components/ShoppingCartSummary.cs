aa8038535ebd6239e6ee14b5f4a4a9d6b272eb82
Mikael <mikael@sehlin.net>
2020-03-03 03:03:21 +0100

    Adding ShoppingCart ViewComponent

---
+++ CandyStore/Components/ShoppingCartSummary.cs
@@ -0,0 +1,33 @@
+﻿using CandyStore.Models;
+using CandyStore.ViewModels;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CandyStore.Components
+{
+    public class ShoppingCartSummary : ViewComponent
+    {
+        private readonly ShoppingCart _shoppingCart;
+
+        public ShoppingCartSummary(ShoppingCart shoppingCart)
+        {
+            _shoppingCart = shoppingCart;
+        }
+
+        public IViewComponentResult Invoke()
+        {
+            _shoppingCart.ShoppingCartItems = _shoppingCart.GetShoppingCartItems();
+
+            var shoppingCartViewModel = new ShoppingCartViewModel
+            {
+                ShoppingCart = _shoppingCart,
+                ShoppingCartTotal = _shoppingCart.GetShoppingCartTotal()
+            };
+
+            return View(shoppingCartViewModel);
+        }
+    }
+}
